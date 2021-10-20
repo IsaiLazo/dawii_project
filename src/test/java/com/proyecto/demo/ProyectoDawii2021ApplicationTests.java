382fc38a2a9e210b3bcc6254001770ee042680ff
IsaiLazo <isalazo_benites@outlook.es>
2021-10-19 21:35:17 -0500

    first commit

---
+++ src/test/java/com/proyecto/demo/ProyectoDawii2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDawii2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
